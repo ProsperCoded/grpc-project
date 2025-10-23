335751af5388e4b5ccceab96e927ca01ef8e49fc
Prosper Coded <enweremproper@gmail.com>
2025-10-23 12:57:19 +0100

    Configures gRPC microservice for products
    Configures the products service as a gRPC microservice.
    
    - feat(products): Sets up gRPC transport with URL, package, and proto path options.
    - fix(products): Updates the default port to avoid conflicts.
    - build: Includes proto files as assets during the build process.

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -6,12 +6,25 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { protobufPackage } from '../../../types/proto/products';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: '0.0.0.0:50051',
+      package: protobufPackage,
+      protoPath: join(__dirname, '../../../proto/products.proto'),
+    },
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
